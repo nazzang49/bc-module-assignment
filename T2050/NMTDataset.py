474c139dd603965b96199d36dc302945a147f841
kimjaehyun <robot032195@gmail.com>
2021-09-14 03:59:35 +0000

    Fix NMT

--- T2050/NMTDataset.py
+++ T2050/NMTDataset.py
@@ -1,6 +1,5 @@
-
-from typing import List, Dict, Tuple, Sequence, Any, Language
-
+from typing import List, Dict, Tuple, Sequence, Any
+from tmp_utils.NMTtools import Language
 """
     해당 모듈에서 빠져 있는 함수    
         1. preprocess
@@ -34,3 +33,35 @@ class NMTDataset(Sequence[Tuple[List[int], List[int]]]):
 
     def __len__(self) -> int:
         return len(self._src)
+
+
+if __name__ == '__main__':
+    from tmp_utils.NMTtools import *
+    print("======Dataloader Test======")
+    english = Language(
+        ['고기를 잡으러 바다로 갈까나', '고기를 잡으러 강으로 갈까나', '이 병에 가득히 넣어가지고요'])
+    korean = Language(['Shall we go to the sea to catch fish ?',
+                      'Shall we go to the river to catch fish', 'Put it in this bottle'])
+    english.build_vocab()
+    korean.build_vocab()
+    dataset = NMTDataset(src=english, tgt=korean)
+
+    batch_size = 3
+    max_pad_len = 5
+    sentence_length = list(
+        map(lambda pair: (len(pair[0]), len(pair[1])), dataset))
+    """
+        dataset의 index 접근으로 인해 idx번째 문장의 src, tgt을 word2idx로 인코딩 된 값들의 길이가 tuple 형태로 짝을 이뤄 list에 저장된다.
+
+        pair[0] = src_sentence : List[int] / pair[1] = tgt_sentence : List[int]
+    """
+    bucketed_batch_indices(
+        sentence_length, batch_size=batch_size, max_pad_len=max_pad_len)
+    dataloader = torch.utils.data.dataloader.DataLoader(dataset, collate_fn=collate_fn, num_workers=2,
+                                                        batch_sampler=bucketed_batch_indices(sentence_length, batch_size=batch_size, max_pad_len=max_pad_len))
+
+    src_sentences, tgt_sentences = next(iter(dataloader))
+    print("Tensor for Source Sentences: \n", src_sentences)
+    print("Tensor for Target Sentences: \n", tgt_sentences)
+
+    print("모든 전처리 과제를 완료했습니다 고생하셨습니다 :)")
